8531d4be782eec4ae2c1a1467fc9cd78e37f50ff
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-04 17:01:00 +0700

    Add login page

---
+++ src/pages/loginPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginForm from '../../components/loginForm';
+
+import './style.css';
+
+const LoginPage = () => {
+  return (
+    <>
+      <h3>Log in with your company account</h3>
+      <LoginForm/>
+    </>  
+  )
+}
+
+export default LoginPage
