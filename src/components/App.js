cd5e1aa29e0d45020d8884ce8ea05fe561ca5cb8
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-06 16:05:18 +0700

    add routing to app

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LoginPage from '../pages/loginPage/loginPage';
+import Homepage from '../pages/homepage/homepage';
+
+import './App.css';
+
+function App() {
+  const userEmail = useSelector(state => state.auth.userEmail);
+
+  return (
+    <Router>
+      <Routes>
+        {!userEmail && <Route path='/' element={<LoginPage />} />}
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
