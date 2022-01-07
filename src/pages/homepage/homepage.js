af292c3acbaf717f76bb5001b849534307c2a5ac
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-07 15:59:06 +0700

    remove redundant Link component in home page

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { signOut } from '../../app/redux';
-import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   const dispatch = useDispatch();
@@ -9,7 +8,6 @@ const Homepage = () => {
   return (
     <>
       <h2>Home</h2>
-      <Link to='/user'>User home</Link>
       <button onClick={() => dispatch(signOut())}>Sign out</button>
     </>  
   )
