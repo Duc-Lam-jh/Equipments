e144a9e8b1611ade2be2bdbae5b215d81e3a9827
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-07 15:49:56 +0700

    add routing based on role

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import { React } from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LoginPage from '../pages/loginPage/loginPage';
 import Homepage from '../pages/homepage/homepage';
+import AdminHomepage from '../pages/homepage/adminHomepage';
 
 import './App.css';
 
 function App() {
-  const userEmail = useSelector(state => state.auth.userEmail);
-  console.log(userEmail);
+  const userEmail = localStorage.getItem("userEmail");
+  const userRole = localStorage.getItem("userRole");
+  
+  const userEmailFromState = useSelector(state => state.auth.userEmail);
+  const userRoleFromState = useSelector(state => state.auth.userRole);
+
   return (
     <Router>
       <Routes>
-        {!userEmail && <Route path='/' element={<LoginPage />} />}
-        <Route path='/' element={<Homepage />} />
+        {!userEmailFromState && <Route path='/' element={<LoginPage />} />}
+        {userRoleFromState==="admin" ?
+        <>
+          <Route path='/' element={<AdminHomepage />} />
+          <Route path='/user' element={<Homepage />} />
+        </> :
+        <>
+          <Route path='/' element={<Homepage />} />
+        </>}
       </Routes>
     </Router>
   );
