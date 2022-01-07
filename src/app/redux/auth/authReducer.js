92c72b335e140369c8723f464be25fd31d6f8372
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-07 15:45:40 +0700

    create actions to handle error, signIn, signOut

--- src/app/redux/auth/authReducer.js
+++ src/app/redux/auth/authReducer.js
@@ -2,14 +2,32 @@ import actionTypes from './authActionTypes';
 
 const initialState = {
   userEmail: null,
+  userRole: null,
+  error: null,
 };
 
 const authReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case actionTypes.SET_ACTIVE_USER:
+  switch (action.type) {
+    case actionTypes.AUTH_SET_ACTIVE_USER:
+      localStorage.setItem("userEmail", action.userEmail);
+      localStorage.setItem("userRole", action.userRole);
       return {
         ...state,
         userEmail: action.userEmail,
+        userRole: action.userRole,
+        error: null,
+      }
+    case actionTypes.AUTH_ERROR:
+      return {
+        ...state,
+        error: action.error,
+        userRole: null
+      }
+    case actionTypes.AUTH_USER_SIGN_OUT:
+      localStorage.clear();
+      return {
+        userEmail: null,
+        userRole: null
       }
     default:
       return state;
