0f474edd48b9962533096a88e0bd825e872b460b
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-07 15:45:07 +0700

    create actions to handle error, signIn, signOut

--- src/app/redux/auth/authActions.js
+++ src/app/redux/auth/authActions.js
@@ -1,16 +1,50 @@
 import actionTypes from './authActionTypes';
 
-const signIn = () => {
-  return {
-    type: actionTypes.SIGN_IN,
-  }
-}
-
 const setActiveUser = (credentials) => {
   return {
-    type: actionTypes.SET_ACTIVE_USER,
+    type: actionTypes.AUTH_SET_ACTIVE_USER,
     userEmail: credentials.email,
+    userRole: credentials.role,
   }
 }
 
-export { signIn, setActiveUser };
+const signOut = () => {
+  return {
+    type: actionTypes.AUTH_USER_SIGN_OUT,
+  }
+}
+
+const setErrorUserNotExist = () => {
+  return {
+    type: actionTypes.AUTH_ERROR,
+    error: "User doesn't exist!",
+  }
+}
+
+const setErrorWrongPassword = () => {
+  return {
+    type: actionTypes.AUTH_ERROR,
+    error: "Wrong password!",
+  }
+}
+
+const signIn = (credentials) => {
+  const uri = process.env.REACT_APP_BASE_API_URL + '/users?q=' + credentials.email;
+  return (dispatch) => {
+    fetch(uri)
+    .then(res => res.json())
+    .then(data => {
+      if(data.length === 0) {
+        dispatch(setErrorUserNotExist());
+      } else {
+        if(credentials.password !== data[0].password) {
+        dispatch(setErrorWrongPassword());
+        } else {
+          dispatch(setActiveUser({...data[0]}));
+        }
+      }
+    })
+  }
+}
+
+export { signIn, signOut, setActiveUser };
