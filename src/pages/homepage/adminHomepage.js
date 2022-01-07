45df3768597374fa76156c16a2c56967398ce0e7
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-07 15:50:50 +0700

    add homepage for admin and user

--- src/pages/homepage/adminHomepage.js
+++ src/pages/homepage/adminHomepage.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { signOut } from '../../app/redux';
 import { Link } from 'react-router-dom';
+import { signOut } from '../../app/redux';
 
-const Homepage = () => {
+const AdminHomepage = () => {
   const dispatch = useDispatch();
 
   return (
     <>
-      <h2>Home</h2>
+      <h2>Admin home</h2>
       <Link to='/user'>User home</Link>
       <button onClick={() => dispatch(signOut())}>Sign out</button>
-    </>  
+    </>
   )
 };
-export default Homepage;
+export default AdminHomepage;
