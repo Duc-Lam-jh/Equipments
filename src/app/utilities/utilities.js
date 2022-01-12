d92614fdc2da87391335d622f9697ca7923a6a2c
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-12 16:48:17 +0700

    shorten isUserLoggedIn function: if logged in then return an object

--- src/app/utilities/utilities.js
+++ src/app/utilities/utilities.js
@@ -2,12 +2,10 @@ export const isUserLoggedIn = () => {
   const userEmail = localStorage.getItem("userEmail");
   const userRole = localStorage.getItem("userRole");
 
-  if(!userEmail || !userRole) {
-    return false;
-  } else {
+  if(userEmail && userRole) {
     return {
       email: userEmail,
       role: userRole
-    }
+    } 
   }
 }
