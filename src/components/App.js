1560a8e3d86eb67084586516d54869c3859f7bea
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-12 14:29:03 +0700

    add code block to check if user data is saved in browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,30 @@
 import { React } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import { isUserLoggedIn } from '../app/utilities/utilities';
+import { setActiveUser } from '../app/redux';
 
 import NotFound from '../pages/NotFoundPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import RootHomepage from '../pages/homepage/RootHomepage';
-import Homepage from '../pages/homepage/Homepage';
-import AdminHomepage from '../pages/homepage/AdminHomepage';
 
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const userData = isUserLoggedIn();
+  if (userData) {
+    dispatch(setActiveUser({ ...userData }));
+  }
+
   const userEmail = useSelector(state => state.auth.userEmail);
-  const userRole = useSelector(state => state.auth.userRole);
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={ <RootHomepage /> } />
+        <Route path='/' element={<RootHomepage />} />
         <Route path='/login' element={!userEmail ? <LoginPage /> : <Navigate to='/' />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
