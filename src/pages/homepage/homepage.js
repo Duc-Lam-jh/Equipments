5f3920b09f7bee65a4fa42b6762bb7a7ae05070a
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-13 16:01:09 +0700

    add DeclareForm route and add dynamic sidebar

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signOut } from '../../app/redux';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
+import DeclareForm from '../../components/DeclareForm/DeclareForm';
+import TestPage from '../TestPage';
 
 import './style.css';
 
 const Homepage = () => {
   const dispatch = useDispatch();
 
+  const listOfPages = [
+    {
+      title: "Devices in possession",
+      path: "/devices"
+    },
+    {
+      title: "Declare possession",
+      path: "/declare-device"
+    },
+    {
+      title: "Request new device",
+      path: "/request-device"
+    },
+  ]
+
   return (
     <>
       <div className='container'>
-        <Sidebar />
-        <button onClick={() => dispatch(signOut())}>Sign out</button>
+        <Sidebar items={listOfPages} />
+        <Routes>
+          <Route path='/' element={<Navigate to='/devices' />} />
+          <Route path='/devices' element={<TestPage />} />
+          <Route path='/declare-device/*' element={<DeclareForm />} />
+        </Routes>
+        <div className='content'>
+          <button onClick={() => dispatch(signOut())}>Sign out</button>
+        </div>
       </div>
     </>
   )
