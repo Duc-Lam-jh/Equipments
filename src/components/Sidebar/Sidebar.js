7c548a7cd66a5250a567c127d0671096461e011d
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-13 15:59:08 +0700

    add navigation sidebar for users

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
+import logo from '../../app/img/logo-white.png';
 
 import SidebarItem from './SidebarItem/SidebarItem';
 
 import './style.css'
 
 class Sidebar extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: props.items,
+    }
+  }
 
+  render() {
+    const sidebarItems = this.state.items.map((item, index) => <NavLink className='item' key={index} to={item.path}><SidebarItem title={item.title} /></NavLink>);
     return <>
       <div className="sidebar">
-        <SidebarItem title="Devices in possession" />
-        <SidebarItem title="Declare possession" />
-        <SidebarItem title="Request new device" />
-
+        <div className='logo'>
+          <img src={logo} width={50}/> 
+          <p>Journey Horizon</p>
+        </div>
+        {sidebarItems}
       </div>
     </>;
   }
