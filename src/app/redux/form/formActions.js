9fd70f4f8e01e89737ec91ccc5f1dbc6ea6576ed
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-14 15:26:07 +0700

    add action to request new device

--- src/app/redux/form/formActions.js
+++ src/app/redux/form/formActions.js
@@ -17,4 +17,23 @@ const declareNewDevice = (formData) => {
   }
 }
 
-export { declareNewDevice }
+const requestNewDevice = (formData) => {
+  const uri = process.env.REACT_APP_BASE_API_URL + '/requests';
+
+  return (dispatch) => {
+    fetch(uri, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    }).then(() => {
+      return true;
+    }).catch(error => {
+      console.log(error);
+      return false;
+    })
+  }
+}
+
+export { declareNewDevice, requestNewDevice }
