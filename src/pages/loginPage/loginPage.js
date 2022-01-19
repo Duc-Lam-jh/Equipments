05d9b1f56505316797e2639f8b8bc2c451be02bf
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-19 16:07:30 +0700

    add login data on login page for testing

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -7,6 +7,13 @@ const LoginPage = () => {
     <div className='content'>
       <h3>Log in with your company account</h3>
       <LoginForm />
+      <p>
+        Admin: test@mail.com - 123456
+        <br />
+        User 1: test2@mail.com - 123456
+        <br />
+        User 2: test3@mail.com - 123456
+      </p>
     </div>
   )
 };
