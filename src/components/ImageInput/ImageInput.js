319d2f2c0c8ac74c19b87ed08bfaf6b814846274
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-25 14:54:06 +0700

    Image input now receives another prop: isThumbnailVisible

--- src/components/ImageInput/ImageInput.js
+++ src/components/ImageInput/ImageInput.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './style.css'
 
-class ImageDropzone extends Component {
-  constructor() {
-    super();
+class ImageInput extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       error: null,
+      isThumbnailVisible: props.isThumbnailVisible
     }
   }
 
@@ -16,7 +17,9 @@ class ImageDropzone extends Component {
       this.setState({error: "File must be a picture!"});
     } else {
       this.setState({error: null});
-      this.updateThumbnail(e.target, image);
+      if(this.state.isThumbnailVisible){
+        this.updateThumbnail(e.target, image);
+      }
     }
 
     const reader = new FileReader();
@@ -56,11 +59,11 @@ class ImageDropzone extends Component {
     return <>
       <div className='input dropzone'>
         <div className='dropzone-before'>Click to upload an image of the device</div>
-        <input name="image" id="image" type="file" onChange={(e) => this.handleChange(e)} accept='image/*' required={true} multiple/>
+        <input name="image" id="image" type="file" onChange={(e) => this.handleChange(e)} accept='image/*' multiple/>
         {error && <div>{error}</div>}
       </div>
     </>;
   }
 }
 
-export default ImageDropzone;
+export default ImageInput;
