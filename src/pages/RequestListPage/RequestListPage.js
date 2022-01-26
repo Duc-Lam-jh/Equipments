a4241bbb23a13d6e9d8dc36fe1e20da8665222f9
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-26 15:52:38 +0700

    add function to fetch user data base on request

--- src/pages/RequestListPage/RequestListPage.js
+++ src/pages/RequestListPage/RequestListPage.js
@@ -7,10 +7,22 @@ const RequestListPage = () => {
   const [requests, setRequests] = useState([]);
 
   useEffect(() => {
+    const getUser = async (id) => {
+      const uri = process.env.REACT_APP_BASE_API_URL + '/users?id=' + id;
+      const userResponse = await fetch(uri);
+      const user = await userResponse.json();
+      return user[0];
+    }
+
     const getPendingRequests = async () => {
       const uri = process.env.REACT_APP_BASE_API_URL + '/requests?status=pending';
       const response = await fetch(uri);
       const requestData = await response.json();
+
+      for(let i = 0; i < requestData.length; i++) {
+        requestData[i].user = await getUser(requestData[i].userId);
+      }
+
       setRequests(requestData);
     }
 
