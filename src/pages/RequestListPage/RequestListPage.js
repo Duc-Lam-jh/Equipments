9a340e47a68c80aa8fe226f53cdce14878485164
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-26 14:18:30 +0700

    Add function to fetch pending requests from database

--- src/pages/RequestListPage/RequestListPage.js
+++ src/pages/RequestListPage/RequestListPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import RequestList from '../../components/RequestList/RequestList';
 
@@ -6,14 +6,30 @@ const RequestListPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [requests, setRequests] = useState([]);
 
-  return(
-    <>
-      <div className='content'>
-        <h1>List of requests</h1>
-        {requests && <RequestList requests={requests} />}
+  useEffect(() => {
+    const getPendingRequests = async () => {
+      const uri = process.env.REACT_APP_BASE_API_URL + '/requests?status=pending';
+      const response = await fetch(uri);
+      const requestData = await response.json();
+      setRequests(requestData);
+    }
+
+    getPendingRequests();
+    setIsLoading(false);
+  }, [])
+
+  if (isLoading) {
+    return <div className='content'><p>loading...</p></div>
+  } else {
+    return (
+      <>
+        <div className='content'>
+          <h1>List of requests</h1>
+          {requests && <RequestList requests={requests} />}
         </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default RequestListPage;
