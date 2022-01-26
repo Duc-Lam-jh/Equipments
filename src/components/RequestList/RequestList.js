caa0a8b2df879001fa8db66ead1eb3c6904afc89
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-26 16:21:19 +0700

    map fetched data to UI, add buttons to handle request

--- src/components/RequestList/RequestList.js
+++ src/components/RequestList/RequestList.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { 
-  FORM_TYPE_DESKTOP, 
-  FORM_TYPE_LAPTOP, 
-  FORM_TYPE_MOUSE, 
-  FORM_TYPE_OTHER, 
-  DEVICE_TYPE_ICON_SIZE } from '../../app/utilities/index';
+import {
+  FORM_TYPE_DESKTOP,
+  FORM_TYPE_LAPTOP,
+  FORM_TYPE_MOUSE,
+  FORM_TYPE_OTHER,
+  DEVICE_TYPE_ICON_SIZE
+} from '../../app/utilities/index';
 import laptopIcon from '../../app/img/laptop-icon.png';
 import desktopIcon from '../../app/img/desktop-icon.png';
 import mouseIcon from '../../app/img/mouse-icon.png';
@@ -17,7 +18,27 @@ const RequestList = (props) => {
   const { requests } = props;
 
   const renderListItem = () => {
+    const requestList = requests.map(request => {
+      return (
+        <div id={'request' + request.id} key={request.id} className='item'>
+          {renderDeviceTypeIcon(request.type)}
+          <div className='info'>
+            <div className='title'>Requester: {request.user.name}</div>
+            <div className='subtitle'>{request.reason}</div>
+          </div>
+          <div className='controls'>
+            <button className='accept'>
+              Accept
+            </button>
+            <button className='decline'>
+              Decline
+            </button>
+          </div>
+        </div>
+      )
+    });
 
+    return requestList;
   }
 
   const renderDeviceTypeIcon = (type) => {
@@ -31,12 +52,12 @@ const RequestList = (props) => {
       case FORM_TYPE_LAPTOP:
         return (
           <div className='device-type'>
-            <img src={laptopIcon} alt='Device type: laptop' width={DEVICE_TYPE_ICON_SIZE} />            
+            <img src={laptopIcon} alt='Device type: laptop' width={DEVICE_TYPE_ICON_SIZE} />
           </div>
         )
       case FORM_TYPE_MOUSE:
         return (
-          <div className='device-type'>           
+          <div className='device-type'>
             <img src={mouseIcon} alt='Device type: mouse' width={DEVICE_TYPE_ICON_SIZE} />
           </div>
         )
@@ -53,11 +74,7 @@ const RequestList = (props) => {
   return (
     <>
       <div className='request-list'>
-        <div className='item'>
-          {renderDeviceTypeIcon('other')}
-          <div className='info'>Laptop</div>
-          <div className='controls'>Laptop</div>
-        </div>
+        {renderListItem()}
       </div>
     </>
   )
