982db3c09be25afd5029db5f6cbbfa1a9c1bfb93
Duc-Lam-jh <duc.lam@journeyh.io>
2022-01-28 09:50:23 +0700

    add functions to render fields based on device type

---
+++ src/components/ItemEdit/EditItemFields.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import { Field } from 'react-final-form';
+import ImageInput from '../ImageInput/ImageInput';
+
+import {
+  FORM_TYPE_DESKTOP, FORM_TYPE_LAPTOP, FORM_TYPE_MOUSE, FORM_TYPE_OTHER
+} from '../../app/utilities/index';
+
+const EditItemFields = (props) => {
+  const { detail, images } = props;
+
+  const renderDesktopFields = () => {
+    return (
+      <>
+        <Field name="brand">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Brand</div>
+              <div className='input'>
+                <input {...input} name="brand" type="text" autoComplete='off' placeholder={detail.brand} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="size">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Size</div>
+              <div className='input'>
+                <input {...input} name="size" type="text" autoComplete='off' placeholder={detail.size} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="configuration">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Configuration</div>
+              <div className='input'>
+                <input {...input} name="configuration" type="text" autoComplete='off' placeholder={detail.configuration} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="image">
+          {() => (
+            <>
+              <div className='title'>Images
+              </div>
+              <label className="image"><ImageInput images={images} onChange={(image) => props.handleAddImage(image)} /></label>
+            </>
+          )}
+        </Field>
+      </>
+    )
+  }
+
+  const renderLaptopFields = () => {
+    return (
+      <>
+        <Field name="brand">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Brand</div>
+              <div className='input'>
+                <input {...input} name="brand" type="text" autoComplete='off' placeholder={detail.brand} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="seriesNumber">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Serial number</div>
+              <div className='input'>
+                <input {...input} name="seriesNumber" type="text" autoComplete='off' placeholder={detail.seriesNumber} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="configuration">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Processor</div>
+              <div className='input'>
+                <input {...input} name="configuration" type="text" autoComplete='off' placeholder={detail.configuration} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="image">
+          {() => (
+            <>
+              <div className='title'>Images
+              </div>
+              <label className="image"><ImageInput images={images} onChange={(image) => props.handleAddImage(image)} /></label>
+            </>
+          )}
+        </Field>
+      </>
+    )
+  }
+
+  const renderMouseFields = () => {
+    return (
+      <>
+        <Field name="brand">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Brand</div>
+              <div className='input'>
+                <input {...input} name="brand" type="text" autoComplete='off' placeholder={detail.brand} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="image">
+          {() => (
+            <>
+              <div className='title'>Images
+              </div>
+              <label className="image"><ImageInput images={images} onChange={(image) => props.handleAddImage(image)} /></label>
+            </>
+          )}
+        </Field>
+      </>
+    )
+  }
+
+  const renderOtherFields = () => {
+    return (
+      <>
+        <Field name="brand">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Brand</div>
+              <div className='input'>
+                <input {...input} name="brand" type="text" autoComplete='off' placeholder={detail.brand} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="description">
+          {({ input, meta }) => (
+            <>
+              <div className='title'>Description</div>
+              <div className='input'>
+                <input {...input} name="description" type="text" autoComplete='off' placeholder={detail.description} required />
+              </div>
+            </>
+          )}
+        </Field>
+
+        <Field name="image">
+          {() => (
+            <>
+              <div className='title'>Images
+              </div>
+              <label className="image"><ImageInput images={images} onChange={(image) => props.handleAddImage(image)} /></label>
+            </>
+          )}
+        </Field>
+      </>
+    )
+  }
+
+  switch (detail.type) {
+    case FORM_TYPE_DESKTOP:
+      return renderDesktopFields();
+    case FORM_TYPE_LAPTOP:
+      return renderLaptopFields();
+    case FORM_TYPE_MOUSE:
+      return renderMouseFields();
+    case FORM_TYPE_OTHER:
+      return renderOtherFields();
+    default:
+      return null;
+  }
+}
+
+export default EditItemFields;
