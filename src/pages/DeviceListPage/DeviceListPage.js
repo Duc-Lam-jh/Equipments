ee26c0baf97d6365a8ab59d1da7ca95b6b012b0a
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 10:12:59 +0700

    initiate DeviceListPage folder and files

---
+++ src/pages/DeviceListPage/DeviceListPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.css';
+
+const DeviceListPage = () => {
+
+  return (
+    <>
+      <div className='content'>
+        <div className='device-list-container'>
+          <div className='item'>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DeviceListPage;
