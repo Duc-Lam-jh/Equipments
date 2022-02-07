2c8070bd67f3877407b5a4dc7da194e68cca4d1e
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 13:51:46 +0700

    add UI template for devices

--- src/components/DeviceList/DeviceList.js
+++ src/components/DeviceList/DeviceList.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const DeviceList = (props) => {
   const { devices } = props;
+
+  useEffect(() => {
+    const renderDevices = () => {
+    
+
+    }
+
+  }, [])
+
 
   return (
     <>
       <div className='device-list-container'>
         <div className='item'>
           <div className='device-type'>Laptop/PC</div>
-          <div className='image'></div>
-          <div className='owner'>Duc Lam</div>
-          <div className='team'>Yin yang</div>
+          <div className='image'
+            style={{
+              backgroundImage: 'url(https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/c419.94.263.263a/p960x960/273049184_2142152845932809_3457391526845144560_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_ohc=YDAaNZ_unSIAX8ET_Az&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT-byjw7pHzq_v2hCJGFIHLtuoLpgNdZFtwJKSWb5gJEVw&oe=6204CA47)',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+            }}>
+          </div>
+          <div className='owner'>Owner: Duc Lam</div>
         </div>
         <div className='item'>
           asdasd
