9c5f54824f4040aa1707a5c85573fc7126e4de18
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 15:00:09 +0700

    add user name to devices in database to aid in fetching device list

--- src/pages/DeviceListPage/DeviceListPage.js
+++ src/pages/DeviceListPage/DeviceListPage.js
@@ -8,22 +8,11 @@ const DeviceListPage = () => {
   const [devices, setDevices] = useState([]);
 
   useEffect(() => {
-    const getUser = async (id) => {
-      const uri = process.env.REACT_APP_BASE_API_URL + '/users?id=' + id;
-      const userResponse = await fetch(uri);
-      const user = await userResponse.json();
-      return user[0];
-    }
-
     const getDevices = async () => {
       const uri = process.env.REACT_APP_BASE_API_URL + '/devices';
       const response = await fetch(uri);
       const devices = await response.json();
       
-      for(let i = 0; i < devices.length; i++) {
-        devices[i].user = await getUser(devices[i].userId);
-      }
-
       setDevices([...devices]);
       setIsLoading(false);
     }
