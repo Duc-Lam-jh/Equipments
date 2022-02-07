dab0fba61270facce3297185ab695889072b64dc
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 10:12:29 +0700

    add DeviceListPage to AdminHomepage

--- src/pages/homepage/adminHomepage.js
+++ src/pages/homepage/adminHomepage.js
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
 import DeviceDetail from '../DeviceDetail/DeviceDetail';
-import DeviceList from '../../components/DeviceList/DeviceList';
+import DeviceListPage from '../DeviceListPage/DeviceListPage';
 import DeviceEditPage from '../DeviceEditPage/DeviceEditPage';
 import RequestListPage from '../RequestListPage/RequestListPage';
 import Test from '../TestPage';
@@ -27,7 +27,7 @@ const AdminHomepage = () => {
       <div className='container'>
         <Routes>
           <Route path='/*' element={<Navigate to='/devices' />} />
-          <Route path='/devices' element={<DeviceList />} />
+          <Route path='/devices' element={<DeviceListPage />} />
           <Route path='/devices/:id' element={<DeviceDetail />} />
           <Route path='/devices/:id/edit' element={<DeviceEditPage />} />
           <Route path='/requests' element={<RequestListPage />} />
