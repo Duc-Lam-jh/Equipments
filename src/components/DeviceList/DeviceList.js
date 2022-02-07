a1fa33c6c986a34726fe35e7201863a08751ac7d
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 14:32:38 +0700

    fetch data and map to UI

--- src/components/DeviceList/DeviceList.js
+++ src/components/DeviceList/DeviceList.js
@@ -22,35 +22,34 @@ const DeviceList = (props) => {
       case FORM_TYPE_OTHER:
         return <div className='device-type'>Other</div>
     }
+  }
 
+  const renderImage = (image) => {
+    // const container = document.createElement('div');
+    // container.style.backgroundImage = 'url(image)';
+    // container.classList.add('image');
+    return <div className='image'>
+      <img src={image} alt='Preview image of device' />
+    </div>;
+  }
+
+  const renderDevices = () => {
+    const deviceList = devices.map(device => {
+      return (
+        <div className='item' key={device.id}>
+          {renderDeviceType(device.type)}
+          {renderImage(device.images[0])}
+          <div className='owner'>Owner: {device.user.name}</div>
+        </div>
+      )
+    });
+    return deviceList;
   }
 
   return (
     <>
       <div className='device-list-container'>
-        <div className='item'>
-          <div className='device-type'>Laptop/PC</div>
-          <div className='image'
-            style={{
-              backgroundImage: 'url(https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/c419.94.263.263a/p960x960/273049184_2142152845932809_3457391526845144560_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_ohc=YDAaNZ_unSIAX8ET_Az&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT-byjw7pHzq_v2hCJGFIHLtuoLpgNdZFtwJKSWb5gJEVw&oe=6204CA47)',
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-            }}>
-          </div>
-          <div className='owner'>Owner: Duc Lam</div>
-        </div>
-        <div className='item'>
-          asdasd
-        </div>
-        <div className='item'>
-          asdasd
-        </div>
-        <div className='item'>
-          asdasd
-        </div>
-        <div className='item'>
-          asdasd
-        </div>
+        {devices && renderDevices()}
       </div>
     </>
   )
