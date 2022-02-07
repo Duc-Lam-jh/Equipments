d3fb98965b2235ab9cba58cb02dda7457f30001e
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 16:04:07 +0700

    add filter to DeviceListPage

--- src/components/ArrayFilter/ArrayFilter.js
+++ src/components/ArrayFilter/ArrayFilter.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import './style.css';
 
-const ArrayFilter = () => {
+const ArrayFilter = (props) => {
+  const { filterList } = props;
 
+  const handleChangeFilter = (type) => {
+    const buttons = document.querySelectorAll('.filter-item');
+    buttons.forEach(item => {
+      if(item.id === type){
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    })
+    props.handleChangeFilter(type);
+  }
+
+  const renderFilter = () => {
+    const filters = filterList.map(item => {
+      return (
+        <div id={item.type} key={item.type} className='filter-item' onClick={() => handleChangeFilter(item.type)}>{item.name}</div>
+      )
+    })
+
+    return filters;
+  }
+
+  return (
+    <>
+      <div className='device-type-picker'>
+        {renderFilter()}
+      </div>
+    </>
+  )
 }
 
 export default ArrayFilter;
