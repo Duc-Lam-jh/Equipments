e06ec22212c66e78e92ca24f709de55f7a980c6b
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-07 13:55:06 +0700

    render device type based on data

--- src/components/DeviceList/DeviceList.js
+++ src/components/DeviceList/DeviceList.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+
+import {
+  FORM_TYPE_LAPTOP,
+  FORM_TYPE_DESKTOP,
+  FORM_TYPE_MOUSE,
+  FORM_TYPE_OTHER
+} from '../../app/utilities/index';
 
 const DeviceList = (props) => {
   const { devices } = props;
 
-  useEffect(() => {
-    const renderDevices = () => {
-    
-
+  const renderDeviceType = (type) => {
+    switch (type) {
+      case FORM_TYPE_DESKTOP:
+        return <div className='device-type'>Desktop</div>
+      case FORM_TYPE_LAPTOP:
+        return <div className='device-type'>Laptop/PC</div>
+      case FORM_TYPE_MOUSE:
+        return <div className='device-type'>Mouse</div>
+      case FORM_TYPE_OTHER:
+        return <div className='device-type'>Other</div>
     }
 
-  }, [])
-
+  }
 
   return (
     <>
