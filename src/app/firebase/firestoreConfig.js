2264e5b0460d22a4a22fd419302b883c6aad4c96
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-11 10:57:08 +0700

    add config to get firebase storage

--- src/app/firebase/firestoreConfig.js
+++ src/app/firebase/firestoreConfig.js
@@ -2,10 +2,12 @@ import {
   getFirestore, 
   collection
  } from 'firebase/firestore';
+ import { getStorage } from 'firebase/storage';
  import { firebaseApp } from './firebaseConfig';
 
  const databaseConnection = getFirestore(firebaseApp);
-
+ 
+ export const storageRef = getStorage(firebaseApp);
  export const devicesCollection = collection(databaseConnection, 'devices');
  export const requestsCollection = collection(databaseConnection, 'requests');
  export const usersCollection = collection(databaseConnection, 'users');
