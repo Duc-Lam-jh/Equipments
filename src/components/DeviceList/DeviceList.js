aea54ea915f9842516be459c239289bd39f208f0
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-16 09:37:35 +0700

    map data to list view UI of device list component

--- src/components/DeviceList/DeviceList.js
+++ src/components/DeviceList/DeviceList.js
@@ -8,12 +8,17 @@ import {
   FORM_TYPE_MOUSE,
   FORM_TYPE_OTHER,
   TOGGLE_VIEW_CARD,
-  TOGGLE_VIEW_LIST
+  TOGGLE_VIEW_LIST,
+  DEVICE_TYPE_ICON_SIZE_SMALL
 } from '../../app/utilities/index';
+import laptopIcon from '../../app/img/laptop-icon.png';
+import desktopIcon from '../../app/img/desktop-icon.png';
+import mouseIcon from '../../app/img/mouse-icon.png';
+import otherIcon from '../../app/img/other-icon.png';
+import rightArrowIcon from '../../app/img/right-arrow-icon.png';
 
 const DeviceList = (props) => {
   const { devices, listStyle } = props;
-  const [view, setView] = useState(listStyle);
 
   const renderDeviceType_CardView = (device) => {
     switch (device.type) {
@@ -28,10 +33,34 @@ const DeviceList = (props) => {
     }
   }
 
-  const renderImage = (image) => {
-    return <div className='image'>
-      <img src={image} alt='Preview image of device' />
-    </div>;
+  const renderDeviceType_ListView = (type) => {
+    switch (type) {
+      case FORM_TYPE_DESKTOP:
+        return (
+          <div className='device-type list-view'>
+            <img src={desktopIcon} alt='Device type: desktop' width={DEVICE_TYPE_ICON_SIZE_SMALL} />
+          </div>
+        )
+      case FORM_TYPE_LAPTOP:
+        return (
+          <div className='device-type list-view'>
+            <img src={laptopIcon} alt='Device type: laptop' width={DEVICE_TYPE_ICON_SIZE_SMALL} />
+          </div>
+        )
+      case FORM_TYPE_MOUSE:
+        return (
+          <div className='device-type list-view'>
+            <img src={mouseIcon} alt='Device type: mouse' width={DEVICE_TYPE_ICON_SIZE_SMALL} />
+          </div>
+        )
+      case FORM_TYPE_OTHER:
+      default:
+        return (
+          <div className='device-type list-view'>
+            <img src={otherIcon} alt='Device type: desktop' width={DEVICE_TYPE_ICON_SIZE_SMALL} />
+          </div>
+        )
+    }
   }
 
   const renderDevices_CardView = () => {
@@ -48,13 +77,46 @@ const DeviceList = (props) => {
     return deviceList;
   }
 
+  const renderDevices_ListView = () => {
+    const deviceList = devices.map(device => {
+      const deviceUri = '/devices/' + device.id;
+      return (
+        <div key={device.id} className='item list-view'>
+          {renderDeviceType_ListView(device.type)}
+          <div className='device-info'>
+            <div className='item'>Brand: {device.brand}</div>
+            <div className='item'>Owner: {device.userName}</div>
+          </div>
+          <Link to={deviceUri} className='device-type list-view'>
+            <img src={rightArrowIcon} alt='Device type: desktop' width={DEVICE_TYPE_ICON_SIZE_SMALL} />
+          </Link>
+        </div>
+      )
+    });
+    return deviceList;
+  }
+
+  const renderImage = (image) => {
+    return <div className='image'>
+      <img src={image} alt='Preview image of device' />
+    </div>;
+  }
+
   const renderList = () => {
     switch (listStyle) {
       case TOGGLE_VIEW_CARD: {
-        return renderDevices_CardView();
+        return (
+          <div className='device-list-container'>
+            {renderDevices_CardView()}
+          </div>
+        );
       }
       case TOGGLE_VIEW_LIST: {
-        return <div>list</div>;
+        return (
+          <div className='device-list-container list-view'>
+            {renderDevices_ListView()}
+          </div>
+        );
       }
       default:
         break;
@@ -63,9 +125,7 @@ const DeviceList = (props) => {
 
   return (
     <>
-      <div className='device-list-container'>
-        {devices && renderList()}
-      </div>
+      {devices && renderList()}
     </>
   )
 }
