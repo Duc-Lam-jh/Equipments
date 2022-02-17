cde8e5cd3cf6afc970e05912134f0a0fda72cfd2
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-17 16:11:58 +0700

    add logic to get Id of metadata collections and save to local storage

--- src/pages/Homepage/RootHomepage.js
+++ src/pages/Homepage/RootHomepage.js
@@ -4,10 +4,20 @@ import { connect } from 'react-redux';
 import Homepage from './Homepage';
 import AdminHomepage from './AdminHomepage';
 import LoginPage from '../LoginPage/LoginPage';
+import { getIdOfCollection } from '../../app/data/metadataActions';
+import { METADATA_NUMBER_OF_DEVICES_KEYWORD, METADATA_NUMBER_OF_PENDING_REQUESTS_KEYWORD } from '../../app/utilities';
 
 class RootHomepage extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount = async () => {
+    const numberOfDevicesCollectionId = await getIdOfCollection(METADATA_NUMBER_OF_DEVICES_KEYWORD);
+    const numberOfRequestCollectionId = await getIdOfCollection(METADATA_NUMBER_OF_PENDING_REQUESTS_KEYWORD);
+
+    localStorage.setItem(METADATA_NUMBER_OF_DEVICES_KEYWORD, numberOfDevicesCollectionId);
+    localStorage.setItem(METADATA_NUMBER_OF_PENDING_REQUESTS_KEYWORD, numberOfRequestCollectionId);
   }
   
   render() {
