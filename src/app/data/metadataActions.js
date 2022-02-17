e69e9f93648039eb891ba41fb53c8f428133c71d
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-17 16:09:54 +0700

    add actions to communicate with metadata collection

---
+++ src/app/data/metadataActions.js
@@ -0,0 +1,62 @@
+import {
+  getDocs, getDoc, doc,
+  query, where, updateDoc
+} from 'firebase/firestore';
+import { metadataCollection } from '../../app/firebase/firestoreConfig';
+import { METADATA_NUMBER_OF_DEVICES_KEYWORD, METADATA_NUMBER_OF_PENDING_REQUESTS_KEYWORD } from '../utilities';
+
+const getIdOfCollection = async (name) => {
+  const deviceQuery = query(metadataCollection, where("name", "==", name));
+  const response = await getDocs(deviceQuery);
+  const documents = response.docs;
+
+  return documents[0].id;
+}
+
+const getNumberOfDevices = async () => {
+  const deviceQuery = query(metadataCollection, where("name", "==", METADATA_NUMBER_OF_DEVICES_KEYWORD));
+  const response = await getDocs(deviceQuery);
+  const documents = response.docs;
+
+  return documents[0].data().value;
+}
+
+const getNumberOfPendingRequests = async () => {
+  const requestQuery = query(metadataCollection, where("name", "==", METADATA_NUMBER_OF_PENDING_REQUESTS_KEYWORD));
+  const response = await getDocs(requestQuery);
+  const documents = response.docs;
+
+  return documents[0].data().value;
+}
+
+const incrementNumberOfDevices = async () => {
+  const collectionId = localStorage.getItem(METADATA_NUMBER_OF_DEVICES_KEYWORD);
+
+  const numberOfDevices = await getNumberOfDevices();
+  try {
+    const documentRef = doc(metadataCollection, collectionId);
+    updateDoc(documentRef, { value: numberOfDevices + 1 });
+  }
+  catch (error) {
+  }
+}
+
+const incrementNumberOfPendingRequests = async () => {
+  const collectionId = localStorage.getItem(METADATA_NUMBER_OF_PENDING_REQUESTS_KEYWORD);
+
+  const numberOfRequests = await getNumberOfPendingRequests();
+  try {
+    const documentRef = doc(metadataCollection, collectionId);
+    updateDoc(documentRef, { value: numberOfRequests + 1 });
+  }
+  catch (error) {
+  }
+}
+
+export {
+  getIdOfCollection,
+  getNumberOfDevices,
+  getNumberOfPendingRequests,
+  incrementNumberOfDevices,
+  incrementNumberOfPendingRequests
+}
