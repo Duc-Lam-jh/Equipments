234e11991309ee02eb540967b2b0c715ba1a17a5
Duc-Lam-jh <duc.lam@journeyh.io>
2022-02-18 13:26:00 +0700

    remove numbered buttons and change into 'see more' function

--- src/components/Paginator/Paginator.js
+++ src/components/Paginator/Paginator.js
@@ -1,16 +1,14 @@
-import React, { createElement, useState } from 'react';
-import { NEXT_PAGE_KEYWORD, PREVIOUS_PAGE_KEYWORD } from '../../app/utilities';
+import React from 'react';
 
 import './style.css';
 
 const Paginator = (props) => {
-  const { isFirstPage, isLastPage } = props;
+  const { isLastPage } = props;
 
   return (
     <div className='paginator-container'>
       <div className='paginator-controls'>
-        <button className='button' onClick={() => props.handleChangePage(PREVIOUS_PAGE_KEYWORD)} disabled={isFirstPage}>Prev</button>
-        <button className='button' onClick={() => props.handleChangePage(NEXT_PAGE_KEYWORD)} disabled={isLastPage}>Next</button>
+        <div className={isLastPage ? 'button disabled' : 'button'} onClick={() => props.handleChangePage()}>See more {'>'}</div>
       </div>
     </div>
   )
